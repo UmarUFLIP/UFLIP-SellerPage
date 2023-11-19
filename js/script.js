0725186289e62f85c6f471c6966f13b1ed53332f
Umar Mian <umarmian@uflip.com>
2023-11-19 09:04:20 -0600

    need to fix mobile responsiveness and add info-slider functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// JavaScript for UFLIP Seller Page
+
 // Set current year for footer
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
@@ -8,10 +10,13 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".mobile-header");
 const menuOpt = document.querySelector(".menu-options");
-const mobNavList = document.querySelector("mobile-nav-list");
+const mobNavList = document.querySelector(".mobile-nav-list");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("mobile-open-nav");
   menuOpt.classList.toggle("open");
+  // The line below is needed to hide pointer events.
   mobNavList.classList.toggle("displayOn");
 });
+
+// The slider information (1 - Fund, 2 - Broadcast, 3 - Buy)
